9b80279b633895e45e71eef6b5c8394c6f024b75
Aleksandr Zh <zhuravlev.a.a@me.com>
2019-09-19 16:46:24 +0300

    объявляю переменные

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var WIZARD = [];
+var wizardsQuantity = 4;
+var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizardsSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColor = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
@@ -8,7 +15,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < wizardsQuantity; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   similarListElement.appendChild(wizardElement);
 }
