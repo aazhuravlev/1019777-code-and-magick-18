2fc7891605468a063a64e2859e055bf79d5625d9
Aleksandr Zh <zhuravlev.a.a@me.com>
2019-09-19 16:24:55 +0300

    отображаю скрытые элементы

--- js/setup.js
+++ js/setup.js
@@ -1,2 +1,14 @@
 'use strict';
 
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  similarListElement.appendChild(wizardElement);
+}
