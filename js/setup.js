53a80c1947a6516d28f0d609d98f8c9763a9decb
Aleksandr Zh <zhuravlev.a.a@me.com>
2019-09-19 19:09:28 +0300

    добавляю ключи

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,7 @@
 
 var wizards = [];
 var wizardsQuantity = 4;
+var KEYS = ['name', 'coatColor', 'eyesColor'];
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
@@ -29,10 +30,6 @@ var getWizardsColors = function (quantity, colors) {
   }
   return wizardsColors;
 };
-
-wizards.name = getWizardsNames(wizardsQuantity, WIZARD_NAMES, WIZARD_SURNAMES);
-wizards.coatColor = getWizardsColors(wizardsQuantity, COAT_COLORS);
-wizards.eyesColor = getWizardsColors(wizardsQuantity, EYES_COLORS);
 
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
