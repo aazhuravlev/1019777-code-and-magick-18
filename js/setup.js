49c84d7bcdf302c3862dfaed9a5290cffda5872d
Aleksandr Zh <zhuravlev.a.a@me.com>
2019-09-19 17:18:55 +0300

    переназываю константы и редактирую функции

--- js/setup.js
+++ js/setup.js
@@ -1,38 +1,38 @@
 'use strict';
 
-var WIZARD = [];
+var wizards = [];
 var wizardsQuantity = 4;
-var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var wizardsSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
-var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var getRandomItem = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-var getWizardsNames = function (wizards, names, surnames) {
+var getWizardsNames = function (quantity, names, surnames) {
   var wizardsNamesSurnames = [];
 
-  for (var i = 0; i < wizards; i++) {
+  for (var i = 0; i < quantity; i++) {
     var wizardName = names.splice(0, 1) + ' ' + surnames.splice(0, 1);
     wizardsNamesSurnames.push(wizardName);
   }
   return wizardsNamesSurnames;
 };
 
-var getWizardsColors = function (wizards, colors) {
+var getWizardsColors = function (quantity, colors) {
   var wizardsColors = [];
 
-  for (var i = 0; i < wizards; i++) {
+  for (var i = 0; i < quantity; i++) {
     wizardsColors.push(getRandomItem(colors));
   }
   return wizardsColors;
 };
 
-WIZARD.name = getWizardsNames(wizardsQuantity, wizardsNames, wizardsSurnames);
-WIZARD.coatColor = getWizardsColors(wizardsQuantity, coatColor);
-WIZARD.eyesColor = getWizardsColors(wizardsQuantity, eyesColor);
+wizards.name = getWizardsNames(wizardsQuantity, WIZARD_NAMES, WIZARD_SURNAMES);
+wizards.coatColor = getWizardsColors(wizardsQuantity, COAT_COLORS);
+wizards.eyesColor = getWizardsColors(wizardsQuantity, EYES_COLORS);
 
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
