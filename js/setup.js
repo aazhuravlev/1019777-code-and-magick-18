b4162e05472f37889f626c4c11ea032a674ea459
Aleksandr Zh <zhuravlev.a.a@me.com>
2019-09-19 16:49:43 +0300

    добавляю функцию для поиска случайного элемента из массива

--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,10 @@ var wizardsSurnames = ['да Марья', 'Верон', 'Мирабелла', '�
 var coatColor = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var getRandomItem = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 
