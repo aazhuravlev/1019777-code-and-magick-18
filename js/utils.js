08aa8e772cc9510d7f05c919c66424512947c580
Aleksandr Zh <zhuravlev.a.a@me.com>
2019-10-09 17:10:46 +0300

    добавляет исправления в объявления функций и переменных

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,21 @@
 'use strict';
 
 (function () {
-  window.getRandomBetween = function (min, max) {
+  var getRandomBetween = function (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  window.getRandomIndex = function (max) {
-    return window.getRandomBetween(0, max - 1);
+  var getRandomIndex = function (max) {
+    return getRandomBetween(0, max - 1);
   };
 
   window.spliceRandomItem = function (arr) {
-    return arr.splice(window.getRandomBetween(arr.length - 1), 1);
+    return arr.splice(getRandomIndex(arr.length - 1), 1);
   };
 
   window.getRandomItem = function (arr) {
-    return arr[window.getRandomBetween(arr.length)];
+    return arr[getRandomIndex(arr.length)];
   };
 })();
